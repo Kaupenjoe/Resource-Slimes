4cfb7d942fe048f4010591b9ece2c1c54652876e
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-04-12 12:52:45 +0200

    Initial commit

---
+++ src/main/java/net/kaupenjoe/resourceslimes/ResourcesSlimes.java
@@ -0,0 +1,45 @@
+package net.kaupenjoe.resourceslimes;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(ResourcesSlimes.MOD_ID)
+public class ResourcesSlimes {
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "resourceslimes";
+
+    public ResourcesSlimes() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+
+    }
+}
